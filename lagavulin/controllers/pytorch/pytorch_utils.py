c124d60bd53c98fa862531201c7f6e47366895af
keng000 <s_flashback@yahoo.co.jp>
2018-06-23 03:04:48 +0900

    Add data augmentation tools

---
+++ lagavulin/controllers/pytorch/pytorch_utils.py
@@ -0,0 +1,13 @@
+
+
+
+import torch
+def load_pytorch_model(model, serialized_model_path):
+    try:
+        model.load_state_dict(torch.load(serialized_model_path))
+    except AssertionError:
+        model.load_state_dict(torch.load(serialized_model_path, map_location=lambda storage, loc: storage))
+
+    return model
+
+
