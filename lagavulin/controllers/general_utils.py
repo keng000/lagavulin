667acfda20fe51b299d84951412e449504e9c822
keng000 <s_flashback@yahoo.co.jp>
2018-07-04 20:23:21 +0900

    Add debug mode to hyper dash manager

--- lagavulin/controllers/general_utils.py
+++ lagavulin/controllers/general_utils.py
@@ -1,10 +1,20 @@
-
 import hyperdash
 from contextlib import contextmanager
+class debugHyperDash(object):
+    def metric(self, name, value):
+        print()
+
+    def end(self):
+        pass
 
 @contextmanager
-def hyper_dash_manager(exp_name):
-    exp = hyperdash.Experiment(exp_name)
+def hyper_dash_manager(exp_name, debug=False):
+
+    if debug:
+        exp = debugHyperDash()
+    else:
+        exp = hyperdash.Experiment(exp_name)
+
     try:
         yield exp
     finally:
