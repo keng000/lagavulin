00ef2a8c60e966610c2465a7bfc517f24553e561
keng000 <4s.flashback@gmail.com>
2018-07-30 23:58:57 +0900

    add pytorch model loader

--- lagavulin/controllers/pytorch/pytorch_utils.py
+++ lagavulin/controllers/pytorch/pytorch_utils.py
@@ -1,9 +1,25 @@
 import torch
-def load_pytorch_model(model, serialized_model_path):
-    try:
-        model.load_state_dict(torch.load(serialized_model_path))
-    except AssertionError:
-        model.load_state_dict(torch.load(serialized_model_path, map_location=lambda storage, loc: storage))
+from pathlib import Path
+from typing import Union
 
-    return model
 
+class PytorchModelLoader:
+    def __init__(self, model: torch.nn.Module):
+        self.model = model
+
+    def get_model(self, trained_model_path=None) -> torch.nn.Module:
+        if trained_model_path:
+            self.model = self._load_serialized_model(self.model, trained_model_path)
+
+        if torch.cuda.is_available():
+            return self.model.cuda()
+
+        return self.model
+
+    def _load_serialized_model(self, model: torch.nn.Module, serialized_model_path: Union[str, Path]):
+        try:
+            model.load_state_dict(torch.load(serialized_model_path))
+        except AttributeError:
+            model.load_state_dict(torch.load(serialized_model_path, map_location=lambda storage, loc: storage))
+
+        return model
