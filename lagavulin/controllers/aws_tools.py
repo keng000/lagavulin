2315fd7711c2de1350b1659edb057d87b2b3b490
keng000 <4s.flashback@gmail.com>
2018-08-09 04:54:04 +0900

    Restricted the try-except

--- lagavulin/controllers/aws_tools.py
+++ lagavulin/controllers/aws_tools.py
@@ -1,29 +1,35 @@
 import boto3
 
 
-def load_config():
-    return {'aws_access_key_id': None, 'aws_secret_access_key': None}
+def load_keys():
+    config = {'aws_access_key_id': None, 'aws_secret_access_key': None}
+    return config['aws_key_id'], config['aws_secret_key']
 
 
-def create_s3_session():
-    config = load_config()
+def create_s3_session(aws_access_key_id, aws_secret_access_key):
     s3 = boto3.client(
         service_name='s3',
-        aws_access_key_id=config['aws_access_key_id'],
-        aws_secret_access_key=config['aws_secret_access_key'])
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key)
     return s3
 
 
 def download_file_from_s3(file_path_in_s3, download_path, session=None, do_retry=False):
+    aws_access_key_id, aws_secret_access_key = load_keys()
 
-    # boto3はスレッドセーフではないため、各スレッド内でセッションを生成する必要がある。
-    # その時、スレッド内でセッション生成に失敗する事例があるため、セッションが生成できるまでトライする。
-    # https://github.com/boto/boto3/issues/454
     while session is None:
+        # より制限された try-except。
+        # ただしBoto3が上げるエラーが不鮮明であるため、これですべてのエラーがキャッチできてるか不明。
+        # もし正常にダウンロードできない場合、下記コメントアウトに代替する。
         try:
-            session = create_s3_session()
-        except:
-            session = None
+            session = create_s3_session(aws_access_key_id, aws_secret_access_key)
+
+        except KeyError as e:
+            if e.args[0] == 'endpoint_resolver':
+                session = None
+
+            else:
+                raise KeyError(e)
 
     try:
         session.download_file('streamed', file_path_in_s3, download_path)
@@ -33,12 +39,12 @@ def download_file_from_s3(file_path_in_s3, download_path, session=None, do_retry
 
         if e.response['Error']['Code'] == "404":
             msg = f"The s3 file does not exists: {file_path_in_s3}"
-            print_log(msg)
+            print(msg)
 
         if do_retry:
             download_file_from_s3(file_path_in_s3, download_path, do_retry=False)
 
-        print_log(f"The s3 file failed to download: {file_path_in_s3}")
+        print(f"The s3 file failed to download: {file_path_in_s3}")
 
 
 def download_with_property_list(dp):
@@ -66,4 +72,4 @@ def concurrent_downloading(download_property_list):
         for idx, _ in enumerate(result):
             print(f"{idx+1}/{len(download_property_list)}")
 
-    print_log("Download Done")
+    print("Download Done")
